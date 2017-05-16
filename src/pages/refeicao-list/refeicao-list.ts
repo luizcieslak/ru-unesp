ff777dadd68e57526f04d87d926420c888c7baf4
luizcieslak <cieslakluiz@gmail.com>
2017-05-16 20:18:57 -0300

    ion card para a refeicao, pegando db do firebase

--- src/pages/refeicao-list/refeicao-list.ts
+++ src/pages/refeicao-list/refeicao-list.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
+
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+import { AuthService } from '../../providers/auth-service';
+
+//moment.js library for handling timestamps
+import * as moment from 'moment';
+import 'moment/locale/pt-br';
 
 /*
   Generated class for the RefeicaoList page.
@@ -13,7 +21,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class RefeicaoListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  refeicoes: FirebaseListObservable<any>; //refeicoes array.
+  loading: Loading;                   //loading component.
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public loadingCtrl: LoadingController, public af: AngularFire,
+    private _auth: AuthService) {
+
+      //create present the loading
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+      
+      //get the song array in a async operation.
+      this.refeicoes = af.database.list('/refeicoes');
+
+      //subscribe to end loading after data is loaded.
+      this.refeicoes.subscribe( snapshot => {
+        this.loading.dismiss();
+      });
+
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RefeicaoListPage');
