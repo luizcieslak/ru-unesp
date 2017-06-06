eac963d23ece57f7d8b1a26b349a8e25403eba2a
luizcieslak <cieslakluiz@gmail.com>
2017-06-06 12:42:09 -0300

    home: confirm functions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,6 +9,7 @@ import * as moment from 'moment';
 import 'moment/locale/pt-br';
 
 import { TimeService } from '../../providers/time-service';
+import { RefeicaoService } from '../../providers/refeicao-service';
 
 //import firebase namespace for functions that aren't in AngularFire2
 import * as firebase from 'firebase/app';
@@ -33,7 +34,7 @@ export class HomePage {
 
   constructor(public navCtrl: NavController, private afAuth: AngularFireAuth,
   public afDB: AngularFireDatabase, public loadingCtrl: LoadingController,
-  public alertCtrl: AlertController, public time: TimeService) {
+  public alertCtrl: AlertController, public time: TimeService, public refeicao: RefeicaoService) {
 
     //create and present the loading
     this.loading = this.loadingCtrl.create();
@@ -90,6 +91,25 @@ export class HomePage {
       return this.shownGroup === group;
   };
 
+  confirmRemove(refeicao: any): void{
+    let confirm = this.alertCtrl.create({
+      title: 'Confirmar Desistência',
+      message: 'Tem certeza que deseja desistir desta refeição? Seu saldo será reembolsado.',
+      buttons: [
+        {
+          text: 'Cancelar',
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            this.remove(refeicao);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
   /**
    * Remove o usuário da refeição, reembolsando-o.
    * @param {any} refeicao A refeição selecionada.
@@ -131,10 +151,41 @@ export class HomePage {
   }
 
   /**
-   * Transfere a compra do usuário para outra refeição.
-   * @param {any} refeicao A refeição selecionada.
+   * Janela de confirmação da transferência de refeições, que mostra uma lista das próximas refeições
+   * @param refeicao refeição escolhida no DOM.
    */
-  transfer(refeicao: any){
+  confirmTransfer(refeicao: any): void{
+    let choose = this.alertCtrl.create();
+    choose.setTitle('Próximas refeições');
+    choose.setSubTitle('Escolha uma data para a transferência');
+
+    choose.addButton('Cancel');
+    choose.addButton({
+      text: 'OK',
+      handler: data => {
+        this.transfer(refeicao, data)
+      }
+    });
+
+    const observable = this.refeicao.nextRefeicoes();
+    observable.subscribe( refeicoes => {
+      refeicoes.forEach( refeicao => {
+        choose.addInput({
+          type: 'radio',
+          label: moment(refeicao.timestamp).calendar(),
+          value: refeicao.$key,
+        });
+      })
+      choose.present();
+    })
+  }
+
+  /**
+   * Tenta transferir a vaga do usuário entre uma refeição e outra.
+   * @param {any} refeicao A refeição de origem.
+   * @param {string} dest A chave da refeição de destino.
+   */
+  transfer(refeicao: any, dest: string){
 
   }
 
